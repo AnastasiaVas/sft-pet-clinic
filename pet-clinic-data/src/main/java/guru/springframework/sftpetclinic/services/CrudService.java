bb1b91aa39eb921ad77a9a5538b3ad418929815a
Nastya <vasilyeva.an1996@gmail.com>
2021-06-07 21:23:23 +0300

    Impl Map based Services, closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/CrudService.java
@@ -6,7 +6,7 @@ public interface CrudService<T, ID> {
 
     Set<T> findAll();
 
-    T findAll(ID id);
+    Set<T> findAll(ID id);
 
     T save(T object);
 
---
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.sftpetclinic.services.map;
+
+import guru.springframework.sftpetclinic.model.Owner;
+import guru.springframework.sftpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Set<Owner> findAll(Long id) {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+}
