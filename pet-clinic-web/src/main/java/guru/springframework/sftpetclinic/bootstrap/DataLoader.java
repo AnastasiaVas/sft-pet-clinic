bb811923a75c254342a210cd629e4b133b734e9d
Nastya <vasilyeva.an1996@gmail.com>
2021-06-16 16:52:06 +0300

    added PetTypes to bootstrap, closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sftpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sftpetclinic/bootstrap/DataLoader.java
@@ -1,7 +1,9 @@
 package guru.springframework.sftpetclinic.bootstrap;
 
 import guru.springframework.sftpetclinic.model.Owner;
+import guru.springframework.sftpetclinic.model.PetType;
 import guru.springframework.sftpetclinic.services.OwnerService;
+import guru.springframework.sftpetclinic.services.PetTypeService;
 import guru.springframework.sftpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Nastya");
